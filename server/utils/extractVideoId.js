2edbb3832a98151cca6e9a80283c482c62dc1071
Rohit-0508 <rohitranaut91@gmail.com>
2025-07-06 16:37:49 +0530

    adjusted videoID code for mobile users

--- server/utils/extractVideoId.js
+++ server/utils/extractVideoId.js
@@ -1,13 +1,27 @@
-exports.getYoutubeVideoID=(url)=>{
-    try{
-        const parsedUrl= new URL(url);
-        if(parsedUrl.hostname === 'www.youtube.com' || parsedUrl.hostname === 'youtube.com') {
-            const params = new URLSearchParams(parsedUrl.search);
-            return params.get('v');
-        }
-        return null;
-    }catch(error){
-        console.error('Invalid URL:', error);
-        return null;    
+exports.getYoutubeVideoID = (url) => {
+  try {
+    const parsedUrl = new URL(url);
+    const hostname = parsedUrl.hostname.replace('www.', '').replace('m.', '');
+
+    // Case 1: youtube.com/watch?v=...
+    if (hostname === 'youtube.com' || hostname === 'youtube-nocookie.com') {
+      const params = new URLSearchParams(parsedUrl.search);
+      return params.get('v');
     }
-}
+
+    // Case 2: youtu.be/dQw4w9WgXcQ
+    if (hostname === 'youtu.be') {
+      return parsedUrl.pathname.split('/')[1];
+    }
+
+    // Case 3: youtube.com/shorts/VIDEO_ID
+    if (hostname === 'youtube.com' && parsedUrl.pathname.startsWith('/shorts/')) {
+      return parsedUrl.pathname.split('/')[2] || parsedUrl.pathname.split('/')[1];
+    }
+
+    return null;
+  } catch (error) {
+    console.error('Invalid YouTube URL:', error);
+    return null;
+  }
+};
