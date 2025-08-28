06bee3c9592e1f656b7046a81e304bfa5433fb2f
Rohit-0508 <rohitranaut91@gmail.com>
2025-08-28 10:57:14 +0530

    Changed Pagination Limit

--- client/src/utils/fetchHistory.js
+++ client/src/utils/fetchHistory.js
@@ -1,6 +1,6 @@
 import api from '../services/api';
 
-const fetchHistory = async (token, page=1, limit=10) => {
+const fetchHistory = async (token, page=1, limit=6) => {
   const res = await api.get(`/history?page=${page}&limit=${limit}`, {
     headers: {
       Authorization: `Bearer ${token}`,
