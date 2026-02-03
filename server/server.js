47f69c7c1843fad0829ab37b2fd10ac579fb91ea
Rohit Verma <rverma@datopic.com>
2026-02-03 15:09:22 +0530

    updated the cors

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   'http://localhost:5173',
+  'https://clipsum.in',
+  'https://www.clipsum.in',
   'https://frolicking-sawine-f50a08.netlify.app',
 ];
 
